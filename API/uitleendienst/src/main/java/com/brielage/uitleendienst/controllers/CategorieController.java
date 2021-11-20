c07d5a21e46e1f878bf3b8b0a8e649c8498b9124
steffen-dc <steffen.de.coster@student.ehb.be>
2021-11-20 16:31:25 +0100

    Update categoriecontroller

--- API/uitleendienst/src/main/java/com/brielage/uitleendienst/controllers/CategorieController.java
+++ API/uitleendienst/src/main/java/com/brielage/uitleendienst/controllers/CategorieController.java
@@ -3,92 +3,96 @@ package com.brielage.uitleendienst.controllers;
 import com.brielage.uitleendienst.APILogger.APILogger;
 import com.brielage.uitleendienst.models.Categorie;
 import com.brielage.uitleendienst.repositories.CategorieRepository;
-import com.brielage.uitleendienst.responses.APIResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping (value = "/categorie")
+@CrossOrigin("http://localhost:8080")
+@RequestMapping(value = "/categorie")
 public class CategorieController {
-    @SuppressWarnings ("SpringJavaAutowiredFieldsWarningInspection")
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
     @Autowired
     private CategorieRepository categorieRepository;
 
-    @PostMapping ("/add")
-    public String add (@RequestBody Categorie categorie)
-            throws
-            JsonProcessingException {
-        APILogger.logRequest("categorie.add", categorie.toString());
-        Map fouten = new LinkedHashMap();
-
-        if (categorie.getNaam()
-                     .isEmpty()) fouten.put("naam_leeg", "");
-        if (categorie.getOmschrijving()
-                     .isEmpty()) fouten.put("omschrijving_leeg", "");
-
-        if (fouten.isEmpty()) {
-            try {
-                Categorie c = categorieRepository.save(categorie);
-                return APIResponse.respondCategorie(c);
-            } catch (Exception e) {return APIResponse.respond(false, e.getMessage());}
-        }
-
-        return APIResponse.respondErrors(fouten);
-    }
-
-    @GetMapping ("/findById/{id}")
-    public String findById (@PathVariable String id)
-            throws
-            JsonProcessingException {
-        APILogger.logRequest("categorie.findById", id);
-        Map fouten = new LinkedHashMap();
-
-        if (id.isEmpty()) fouten.put("id_leeg", "");
-
-        if (fouten.isEmpty()) {
-            try {
-                Optional<Categorie> c = categorieRepository.findById(id);
-                if (c.isPresent()) return APIResponse.respondCategorie(c.get());
-                return APIResponse.respond(false, "geen_categorie_gevonden");
-            } catch (Exception e) {return APIResponse.respond(false, e.getMessage());}
-        }
-
-        return APIResponse.respondErrors(fouten);
-    }
-
-    @GetMapping ("/findByNaam/{naam}")
-    public String findByNaam (@PathVariable String naam)
-            throws
-            JsonProcessingException {
-        APILogger.logRequest("categorie.findByNaam", naam);
-        Map fouten = new LinkedHashMap();
-
-        if (naam.isEmpty()) fouten.put("naam_leeg", "");
-
-        if (fouten.isEmpty()) {
-            try {
-                Optional<Categorie> c = categorieRepository.findByNaam(naam);
-                if (c.isPresent()) return APIResponse.respondCategorie(c.get());
-                return APIResponse.respond(false, "geen_categorie_gevonden");
-            } catch (Exception e) {return APIResponse.respond(false, e.getMessage());}
-        }
-
-        return APIResponse.respondErrors(fouten);
-    }
-
-    @GetMapping ("/findAll")
-    public String findAll ()
-            throws
-            JsonProcessingException {
+    @RequestMapping(value = {"/", ""}, method = RequestMethod.GET)
+    public List<Categorie> findAll() {
         APILogger.logRequest("categorie.findAll");
-        List<Categorie> c = categorieRepository.findAll();
-        if (c.isEmpty()) return APIResponse.respond(false, "geen_categorie_gevonden");
-        return APIResponse.respondCategorie(c);
+        return categorieRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findById(@PathVariable String id) {
+        APILogger.logRequest("categorie.findById", id);
+        Optional<Categorie> c = categorieRepository.findById(id);
+
+        if (c.isPresent())
+            return ResponseEntity.ok().body(c.get());
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping (value = { "/", "" })
+    public ResponseEntity create(@RequestBody Categorie categorie) {
+        APILogger.logRequest("categorie.add", categorie.getId());
+        try {
+            if (!validateCategorie(categorie))
+                return ResponseEntity.badRequest().build();
+
+            Categorie c = categorieRepository.save(categorie);
+
+            return new ResponseEntity(c, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping (value = "/{id}")
+    public ResponseEntity put (@PathVariable String id, @RequestBody Categorie categorie) {
+        APILogger.logRequest("categorie.put", id);
+        try {
+            if (!validateCategorieId(categorie))
+                return ResponseEntity.badRequest().build();
+
+            Optional<Categorie> c = categorieRepository.findById(id);
+
+            if (c.isEmpty())
+                return ResponseEntity.notFound().build();
+
+            categorie.setId(c.get().getId());
+            Categorie result = categorieRepository.save(categorie);
+            return new ResponseEntity(result, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping (value = "/{id}")
+    public ResponseEntity delete (@PathVariable String id) {
+        APILogger.logRequest("categorie.delete", id);
+        try {
+            Optional<Categorie> c = categorieRepository.findById(id);
+
+            if (c.isEmpty())
+                return ResponseEntity.badRequest().build();
+
+            categorieRepository.delete(c.get());
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private boolean validateCategorieId(Categorie c) {
+        if (c.getId().isEmpty())
+            return false;
+        return validateCategorie(c);
+    }
+
+    private boolean validateCategorie(Categorie c) {
+        return !c.getNaam().isEmpty();
     }
 }
