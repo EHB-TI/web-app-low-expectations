f4819d28aaf34010924396b9f43ce50548deabf4
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-20 16:12:48 +0100

    Web: Adding ViewComponent to render Authentication

---
+++ WEB/WebApplication_Uitleendienst/ViewComponents/AuthenticationViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_Uitleendienst.Models.ViewModels;
+
+namespace WebApplication_Uitleendienst.ViewComponents {
+    public class AuthenticationViewComponent : ViewComponent {
+
+        public AuthenticationViewComponent(){
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(){
+           var model = new HomeViewModel();          
+            return View("_LoginPartial", model);
+        }
+    }
+
+}
