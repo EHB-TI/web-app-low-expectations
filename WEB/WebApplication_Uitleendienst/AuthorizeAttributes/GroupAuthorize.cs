28cf7c28c33d05f1c89703ee8e47b7b485814a88
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-21 13:07:26 +0100

    Web: Adding GroupAuthorisationFilter

---
+++ WEB/WebApplication_Uitleendienst/AuthorizeAttributes/GroupAuthorize.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Uitleendienst.AuthorizeAttributes {
+    public class GroupAuthorize : AuthorizeAttribute, IAuthorizationFilter {
+        private string GroupName { get; set; }
+
+        public GroupAuthorize(string groupName) => GroupName = groupName;
+        public void OnAuthorization(AuthorizationFilterContext context) {
+            var userGroups = context.HttpContext.User?.Claims.FirstOrDefault(s => s.Type.Equals("cognito:groups"));
+            if(userGroups != null) {
+                // check wether the group matches the request
+                if(userGroups.Value == GroupName)
+                    return;
+            }
+
+            //Return based on logic
+            context.Result = new UnauthorizedResult();
+        }
+
+    }
+}
