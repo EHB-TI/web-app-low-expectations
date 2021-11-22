5ef419f7557ec8ea232f0ff81f7c1187daaa17b7
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-22 09:47:06 +0100

    Web: Area ADMIN voorzien

---
+++ WEB/WebApplication_Uitleendienst/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_Uitleendienst.AuthorizeAttributes;
+
+namespace WebApplication_Uitleendienst.Areas.Admin.Controllers {
+    [GroupAuthorize("Admins-WebApplication")]
+    public class BaseAdminController : Controller {
+
+    }
+}
---
+++ WEB/WebApplication_Uitleendienst/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Uitleendienst.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class HomeController : BaseAdminController {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
