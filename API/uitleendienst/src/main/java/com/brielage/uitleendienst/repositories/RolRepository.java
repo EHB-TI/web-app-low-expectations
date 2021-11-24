cbc53852f46f33fe5ff21ce5baf18e8995b0ce31
Jens Wielfaert <jens.wielfaert@student.ehb.be>
2021-11-24 19:58:32 +0100

    additions and adjustments to Rol

--- API/uitleendienst/src/main/java/com/brielage/uitleendienst/repositories/RolRepository.java
+++ API/uitleendienst/src/main/java/com/brielage/uitleendienst/repositories/RolRepository.java
@@ -8,10 +8,13 @@ import java.util.List;
 import java.util.Optional;
 
 @EnableScan
-public interface RolRepository extends CrudRepository<Rol, String> {
-    List<Rol> findAll();
+public interface RolRepository
+        extends CrudRepository<Rol, String> {
+    List<Rol> findAll ();
 
     Optional<Rol> findById (String id);
 
     Optional<Rol> findByNaam (String naam);
+
+    List<Rol> findAllByNaamIsIn (List<String> naam);
 }
