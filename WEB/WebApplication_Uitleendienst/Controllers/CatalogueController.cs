49a7457c9dd870e865ab6ce88a60afb3afe0842c
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-25 15:01:18 +0100

    Web: Catalogue Implentation

---
+++ WEB/WebApplication_Uitleendienst/Controllers/CatalogueController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_Uitleendienst.Services.Interfaces;
+
+namespace WebApplication_Uitleendienst.Controllers {
+    public class CatalogueController : Controller {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IBaseService<Categorie> _categorieService;
+        private readonly IBaseService<UitleenbaarItem> _uitleenbaarItemService;
+        public CatalogueController(IBaseService<Categorie> catService, IBaseService<UitleenbaarItem> uitleenbaarItemService) {
+            _categorieService = catService;
+            _uitleenbaarItemService = uitleenbaarItemService;
+        }
+
+        public IActionResult Catalogue(int categoryId) {
+          var product =  _uitleenbaarItemService.Get(categoryId);
+        }
+    }
+}
