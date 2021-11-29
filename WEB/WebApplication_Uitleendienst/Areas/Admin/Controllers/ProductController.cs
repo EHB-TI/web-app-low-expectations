3255d4a2e8e8b322640164ea5dc48cbc27a3ea45
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-29 22:01:40 +0100

    Web: Product management admin

---
+++ WEB/WebApplication_Uitleendienst/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_Uitleendienst.Models.ViewModels.Categories;
+using WebApplication_Uitleendienst.Services.Interfaces;
+
+namespace WebApplication_Uitleendienst.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class ProductController : BaseAdminController {
+        private readonly IBaseService<Categorie> _categoryService;
+        public ProductController(IBaseService<Categorie> catService) {
+            _categoryService = catService;
+        }
+        public IActionResult Index() {
+            var model = new CategoryViewModel();
+            model.Categories = _categoryService.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        public IActionResult Detail(string Id) {
+            var category = _categoryService.GetAll(cache: true).FirstOrDefault(s => s.Id == Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(Categorie cat) {
+            var entity = await _categoryService.Save(cat);
+            return RedirectToAction("index");
+        }
+    }
+}
