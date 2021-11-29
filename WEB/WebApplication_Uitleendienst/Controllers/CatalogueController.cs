8f2fe260725fb511f5064a582631e0405dbea47c
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-29 11:09:11 +0100

    Web: Catalogue update naar detail pagina

--- WEB/WebApplication_Uitleendienst/Controllers/CatalogueController.cs
+++ WEB/WebApplication_Uitleendienst/Controllers/CatalogueController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication_Uitleendienst.Models;
 using WebApplication_Uitleendienst.Models.ViewModels.Catalogue;
+using WebApplication_Uitleendienst.Models.ViewModels.Identity;
 using WebApplication_Uitleendienst.Services.Interfaces;
 
 namespace WebApplication_Uitleendienst.Controllers {
@@ -30,17 +34,56 @@ namespace WebApplication_Uitleendienst.Controllers {
             model.BeschikbareItems = _beschikbaarItemService.GetAll("uitleenbaarItemId", item.Id);
             model.BeschikbareItems.ToList().ForEach(s => {
                 model.TotalStock += (int)s.AantalTotaal;
-                model.Magazijnen.Add( _magazijnService.Get(s.MagazijnId));
+                model.Magazijnen.Add(_magazijnService.Get(s.MagazijnId));
             });
 
             return View(model);
         }
         public IActionResult Catalogue(string categoryId) {
             var model = new CatalogueViewModel();
-            model.Products = _uitleenbaarItemService.GetAll("categorieId", categoryId);
-            //TEST model.Products = ItemList;
+            if (categoryId != null)
+                model.Products = _uitleenbaarItemService.GetAll("categorieId", categoryId);
+            else
+                model.Products = _uitleenbaarItemService.GetAll();
             model.Categories = _categorieService.GetAll();
             return View(model);
+        }
+
+        public string AddToCart(string productId, string magazijnId, string amount) {
+            if (!User.Identity.IsAuthenticated)
+                return "not_authenticated";
+            try {
+                var cart = new List<CartItem>();
+                var cartItem = new CartItem {
+                    Amount = amount,
+                    ProductId = productId,
+                    MagazijnId = magazijnId,
+                    UserId = new UserInfoPageModel(HttpContext).Username
+                };
+                var currentCart = HttpContext.Session.GetString("Cart");
+
+                if (!string.IsNullOrEmpty(currentCart)) {
+                    // when there is info in current cart
+                    // use that cartobject as base
+                    cart = JsonConvert.DeserializeObject<List<CartItem>>(currentCart);
+
+                }
+
+                // check for any existing occurences
+                // if so, add the old amount to the new amount, and remove the occurence from list
+                var existingCartItem = cart.Where(s => s.ProductId == cartItem.ProductId && s.MagazijnId == cartItem.MagazijnId).FirstOrDefault();
+                if (existingCartItem != null){
+                    cartItem.Amount = (int.Parse(cartItem.Amount) + int.Parse(existingCartItem.Amount)).ToString();
+                    cart.Remove(existingCartItem);
+                }
+                cart.Add(cartItem);
+                HttpContext.Session.SetString("Cart", JsonConvert.SerializeObject(cart));
+
+                return "success";
+            } catch (Exception ex) {
+                return ex.Message;
+            }
+
         }
     }
 }
