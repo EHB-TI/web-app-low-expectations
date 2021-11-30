ee3e8b6cc916cef50c87dc254cfdfae5433af4ae
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-30 21:15:30 +0100

    Web: Magazijn CRUD admin

---
+++ WEB/WebApplication_Uitleendienst/Areas/Admin/Controllers/MagazijnController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_Uitleendienst.Models.ViewModels.Magazijns;
+using WebApplication_Uitleendienst.Services.Interfaces;
+
+namespace WebApplication_Uitleendienst.Areas.Admin.Controllers {
+    [Area("Admin")]
+    public class MagazijnController : BaseAdminController {
+        private readonly IBaseService<Magazijn> _magazijnService;
+       
+        public MagazijnController(IBaseService<Magazijn> magazijnService) {
+            _magazijnService = magazijnService;
+        }
+        public IActionResult Index() {
+            var model = new MagazijnViewModel(); 
+            model.Magazijnen = _magazijnService.GetAll();
+            return View(model);
+        }
+
+         public IActionResult Create() {
+            return View();
+        }
+
+        public IActionResult Detail(string Id) {
+            var category = _magazijnService.Get(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(Magazijn mag) {
+            var entity = await _magazijnService.Save(mag);
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditPost(Magazijn mag) {
+            var entity = await _magazijnService.Update(mag);
+            return RedirectToAction("index");
+        }
+
+    }
+}
---
+++ WEB/WebApplication_Uitleendienst/Models/ViewModels/Magazijns/MagazijnViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Uitleendienst.Models.ViewModels.Magazijns {
+    public class MagazijnViewModel {
+        public IEnumerable<Magazijn> Magazijnen { get; set; }
+    }
+}
