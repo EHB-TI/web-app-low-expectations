8a557ccd603987d13ddbc0ef5ea71d10b8a32bf7
Michaël Bracké <michael.bracke@becosoft.be>
2021-11-30 21:15:42 +0100

    Web: Produc CRUD admin

--- WEB/WebApplication_Uitleendienst/Areas/Admin/Controllers/ProductController.cs
+++ WEB/WebApplication_Uitleendienst/Areas/Admin/Controllers/ProductController.cs
@@ -4,33 +4,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication_Uitleendienst.Models.ViewModels.Categories;
+using WebApplication_Uitleendienst.Models.ViewModels.Product;
 using WebApplication_Uitleendienst.Services.Interfaces;
 
 namespace WebApplication_Uitleendienst.Areas.Admin.Controllers {
     [Area("Admin")]
     public class ProductController : BaseAdminController {
         private readonly IBaseService<Categorie> _categoryService;
-        public ProductController(IBaseService<Categorie> catService) {
+        private readonly IBaseService<UitleenbaarItem> _uitleenbaarItemService;
+        private readonly IBaseService<BeschikbaarItem> _beschikbaarItemService;
+        private readonly IBaseService<Magazijn> _magazijnItemService;
+        public ProductController(IBaseService<Categorie> catService, IBaseService<UitleenbaarItem> uitleenbaarItemService, IBaseService<BeschikbaarItem> beschikbaarItemService, IBaseService<Magazijn> magazijnItemService) {
             _categoryService = catService;
+            _uitleenbaarItemService = uitleenbaarItemService;
+            _beschikbaarItemService = beschikbaarItemService;
+            _magazijnItemService = magazijnItemService;
         }
         public IActionResult Index() {
-            var model = new CategoryViewModel();
-            model.Categories = _categoryService.GetAll();
+            var model = new ProductViewModel();
+            model.Products = _uitleenbaarItemService.GetAll();
             return View(model);
         }
 
-        public IActionResult Create() {
+        public IActionResult CreateUitleenbaarItem() {
             return View();
         }
 
+        public IActionResult CreateBeschikbaarItem() {
+            var model = new ProductViewModel();
+            model.Products = _uitleenbaarItemService.GetAll();
+            model.Magazijnen = _magazijnItemService.GetAll();
+            return View(model);
+        }
+
         public IActionResult Detail(string Id) {
-            var category = _categoryService.GetAll(cache: true).FirstOrDefault(s => s.Id == Id);
+            var category = _uitleenbaarItemService.Get(Id);
             return View(category);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreatePost(Categorie cat) {
-            var entity = await _categoryService.Save(cat);
+        public async Task<IActionResult> CreatePost(UitleenbaarItem prod) {
+            var entity = await _uitleenbaarItemService.Save(prod);
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePostBeschikbaar(ProductViewModel model) {
+            var entity = await _beschikbaarItemService.Save(model.BeschikbaarItem);
             return RedirectToAction("index");
         }
     }
---
+++ WEB/WebApplication_Uitleendienst/Models/ViewModels/Product/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Uitleendienst.Models.ViewModels.Product {
+    public class ProductViewModel {
+        public IEnumerable<UitleenbaarItem> Products { get; set; }
+        public IEnumerable<Magazijn> Magazijnen { get; set; }
+        public BeschikbaarItem BeschikbaarItem { get; set; }
+    }
+}
