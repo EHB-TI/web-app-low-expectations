b7a152a58972eed9b69b74ecb1087d9ccd6f73e9
Michaël Bracké <michael.bracke@becosoft.be>
2021-12-02 18:20:21 +0100

    Web: Update services

--- WEB/WebApplication_Uitleendienst/Services/BaseService.cs
+++ WEB/WebApplication_Uitleendienst/Services/BaseService.cs
@@ -13,6 +13,13 @@ using WebApplication_Uitleendienst.Models;
 using WebApplication_Uitleendienst.Services.Interfaces;
 
 namespace WebApplication_Uitleendienst.Services {
+
+
+    //INFO STATUSCODES
+    // 401 -> geen auth header aanwezig terwijl api deze wel verwacht
+    // 400 -> bad requests (params, caps..)
+    // 403 -> wel auth header maar niet juiste groepen rechten om informatie krijgen
+
     public class BaseService<TEntity> : IBaseService<TEntity> where TEntity : class {
         private string BaseUrl => "http://" + Website.API_HOST + "/";
 
@@ -20,50 +27,74 @@ namespace WebApplication_Uitleendienst.Services {
         public BaseService(IMemoryCache cache) {
             _cache = cache;
         }
-        public void Delete<TEntity1>(int id) {
+        public void Delete<TEntity1>(int id, string token = null) {
             throw new NotImplementedException();
         }
 
-        public bool DeleteByProperty(Expression<Func<TEntity, bool>> predicate) {
+        public bool DeleteByProperty(Expression<Func<TEntity, bool>> predicate, string token = null) {
             throw new NotImplementedException();
         }
 
 
-        public TEntity Get(string propertyValue = null, bool cache = false) {
+        public TEntity Get(string propertyName = null, string propertyValue = null, bool cache = false, string token = null) {
+
             var url = BaseUrl + typeof(TEntity).Name.ToLower();
-            if (!string.IsNullOrEmpty(propertyValue))
+
+            if (string.IsNullOrEmpty(propertyName) && !string.IsNullOrEmpty(propertyValue))
                 url += $"/{propertyValue}";
+            if (!string.IsNullOrEmpty(propertyName) && !string.IsNullOrEmpty(propertyValue))
+                url += $"?{propertyName}={propertyValue}";
+
             var key = typeof(TEntity).Name + "_Get_" + DateTime.Now.ToString("yy-MM-dd");
 
             if (!_cache.TryGetValue(key, out TEntity item) || !cache) {
-                var request = WebRequest.Create(url);
-                request.Method = "GET";
-                var response = request.GetResponse();
-                using (Stream dataStream = response.GetResponseStream()) {
-                    // Open the stream using a StreamReader for easy access.
-                    var reader = new StreamReader(dataStream);
-                    // Read the content.
-                    var responseFromServer = reader.ReadToEnd();
-                    // convert to entity
-                    _cache.Set(key, JsonConvert.DeserializeObject<TEntity>(responseFromServer));
+                try {
+                    var request = WebRequest.Create(url);
+                    request.Method = "GET";
+                    if (!string.IsNullOrEmpty(token))
+                        request.Headers.Add("Authorization", token);
+                    else
+                        request.Headers.Add("Authorization", "");
+                    request.Headers.Add("Origin", Website.HOST_NAME);
+                    var response = request.GetResponse();
+                    using (Stream dataStream = response.GetResponseStream()) {
+                        // Open the stream using a StreamReader for easy access.
+                        var reader = new StreamReader(dataStream);
+                        // Read the content.
+                        var responseFromServer = reader.ReadToEnd();
+                        // convert to entity
+                        _cache.Set(key, JsonConvert.DeserializeObject<TEntity>(responseFromServer));
+                    }
+                } catch (Exception ex) {
+                    return null;
                 }
             }
 
             return _cache.Get<TEntity>(key);
         }
 
-        public IEnumerable<TEntity> GetAll(string propertyName = null, string propertyValue = null, bool cache = false) {
+        public IEnumerable<TEntity> GetAll(string propertyName = null, string propertyValue = null, bool cache = false, string token = null) {
 
             var url = BaseUrl + typeof(TEntity).Name.ToLower();
-            if (!string.IsNullOrEmpty(propertyValue))
+
+            if (string.IsNullOrEmpty(propertyName) && !string.IsNullOrEmpty(propertyValue))
+                url += $"/{propertyValue}";
+            if (!string.IsNullOrEmpty(propertyName) && !string.IsNullOrEmpty(propertyValue))
                 url += $"?{propertyName}={propertyValue}";
 
             var key = typeof(TEntity).Name + "_GetAll_" + DateTime.Now.ToString("yy-MM-dd");
+
+
 
             if (!_cache.TryGetValue(key, out IEnumerable<TEntity> items) || !cache) {
                 try {
                     var request = WebRequest.Create(url);
                     request.Method = "GET";
+                    if (!string.IsNullOrEmpty(token))
+                        request.Headers.Add("Authorization", token);
+                    else
+                        request.Headers.Add("Authorization", "");
+                    request.Headers.Add("Origin", Website.HOST_NAME);
                     var response = request.GetResponse();
                     using (Stream dataStream = response.GetResponseStream()) {
                         // Open the stream using a StreamReader for easy access.
@@ -81,33 +112,91 @@ namespace WebApplication_Uitleendienst.Services {
             return _cache.Get<IEnumerable<TEntity>>(key);
         }
 
-        public async Task<TEntity> Save(TEntity item, string customEntity = null) {
+        public async Task<TEntity> Save(TEntity item, string customEntity = null, string token = null) {
             var url = BaseUrl;
             if (customEntity == null)
                 url += typeof(TEntity).Name.ToLower();
             else
                 url += customEntity;
 
+
+            var request = WebRequest.Create(url);
+            request.Method = "POST";
+            if (!string.IsNullOrEmpty(token))
+                request.Headers.Add("Authorization", token);
+            else
+                request.Headers.Add("Authorization", "");
+            request.Headers.Add("Origin", Website.HOST_NAME);
+            request.ContentType = "application/json";
+
+
             var JsonData = JsonConvert.SerializeObject(item);
 
-            using (var client = new HttpClient()) {
-                var response = await client.PostAsync(url, new StringContent(JsonData, Encoding.UTF8, "application/json"));
-                var data = await response.Content.ReadAsStringAsync();
-                if (data != null)
-                    return JsonConvert.DeserializeObject<TEntity>(data);
+            var requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+            requestWriter.Write(JsonData);
+            requestWriter.Close();
+
+            try {
+                var webResponse = request.GetResponse();
+                var webStream = webResponse.GetResponseStream();
+                var responseReader = new StreamReader(webStream);
+                var response = responseReader.ReadToEnd();
+                if (response != null)
+                    return JsonConvert.DeserializeObject<TEntity>(response);
+                responseReader.Close();
+
+            } catch (Exception ex) {
+                return null;
             }
+
             return null;
         }
 
-        public void Save(IEnumerable<TEntity> Items) {
+        public async Task<List<TEntity>> SaveAll(List<TEntity> Items, string customEntity = null, string token = null) {
+             var url = BaseUrl;
+            if (customEntity == null)
+                url += typeof(TEntity).Name.ToLower();
+            else
+                url += customEntity;
+
+
+            var request = WebRequest.Create(url);
+            request.Method = "POST";
+            if (!string.IsNullOrEmpty(token))
+                request.Headers.Add("Authorization", token);
+            else
+                request.Headers.Add("Authorization", "");
+            request.Headers.Add("Origin", Website.HOST_NAME);
+            request.ContentType = "application/json";
+
+
+            var JsonData = JsonConvert.SerializeObject(Items);
+
+            var requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+            requestWriter.Write(JsonData);
+            requestWriter.Close();
+
+            try {
+                var webResponse = request.GetResponse();
+                var webStream = webResponse.GetResponseStream();
+                var responseReader = new StreamReader(webStream);
+                var response = responseReader.ReadToEnd();
+                if (response != null)
+                    return JsonConvert.DeserializeObject<List<TEntity>>(response);
+                responseReader.Close();
+
+            } catch (Exception ex) {
+                return null;
+            }
+
+            return null;
+        }
+
+        public void Update(IEnumerable<TEntity> Items, string token = null) {
             throw new NotImplementedException();
         }
 
-        public void Update(IEnumerable<TEntity> Items) {
-            throw new NotImplementedException();
-        }
-
-        public async Task<TEntity> Update(TEntity item, string customEntity = null) {
+        public async Task<TEntity> Update(TEntity item, string customEntity = null, string token = null) {
             var url = BaseUrl;
             if (customEntity == null)
                 url += typeof(TEntity).Name.ToLower();
@@ -124,5 +213,6 @@ namespace WebApplication_Uitleendienst.Services {
             }
             return null;
         }
+
     }
 }
--- WEB/WebApplication_Uitleendienst/Services/Interfaces/IBaseService.cs
+++ WEB/WebApplication_Uitleendienst/Services/Interfaces/IBaseService.cs
@@ -7,14 +7,14 @@ using WebApplication_Uitleendienst.Models;
 
 namespace WebApplication_Uitleendienst.Services.Interfaces {
     public interface IBaseService<TEntity> where TEntity : class {
-        public void Update(IEnumerable<TEntity> Items);
-        public Task<TEntity> Update(TEntity item, string customEntity = null);
-        public Task<TEntity> Save(TEntity item, string customEntity = null);
-        public void Save(IEnumerable<TEntity> Items);
-        public TEntity Get(string propertyValue = null, bool cache = false);
-        public void Delete<TEntity>(int id);
-        public bool DeleteByProperty(Expression<Func<TEntity, bool>> predicate);
-        public IEnumerable<TEntity> GetAll(string propertyName = null, string propertyValue = null, bool cache = false);
+        public void Update(IEnumerable<TEntity> Items, string token = null);
+        public Task<TEntity> Update(TEntity item, string customEntity = null, string token = null);
+        public Task<TEntity> Save(TEntity item, string customEntity = null, string token = null);
+        public Task<List<TEntity>> SaveAll(List<TEntity> Items, string customEntity = null, string token = null);
+        public TEntity Get(string propertyName = null, string propertyValue = null, bool cache = false, string token = null);
+        public void Delete<TEntity>(int id, string token = null);
+        public bool DeleteByProperty(Expression<Func<TEntity, bool>> predicate, string token = null);
+        public IEnumerable<TEntity> GetAll(string propertyName = null, string propertyValue = null, bool cache = false, string token = null);
 
     }
 }
