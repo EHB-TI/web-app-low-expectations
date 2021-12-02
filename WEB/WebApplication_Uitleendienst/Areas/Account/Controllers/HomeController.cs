6006a521586e8413893952c0ce06946dcfe4d421
Michaël Bracké <michael.bracke@becosoft.be>
2021-12-02 18:19:41 +0100

    Web: Add Account area

---
+++ WEB/WebApplication_Uitleendienst/Areas/Account/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Uitleendienst.Areas.Identity {
+    [Area("Account")]
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
