af718fda81869410579af4519ca225bd5f79345e
Jens Wielfaert <jens.wielfaert@student.ehb.be>
2021-12-02 17:41:35 +0100

    updated JWTChecker

--- API/uitleendienst/src/main/java/com/brielage/uitleendienst/authorization/JWTChecker.java
+++ API/uitleendienst/src/main/java/com/brielage/uitleendienst/authorization/JWTChecker.java
@@ -6,18 +6,24 @@ import com.brielage.uitleendienst.tools.APILogger;
 import java.util.Base64;
 
 public class JWTChecker {
-    String             token;
-    String[]           chunks;
-    Base64.Decoder     decoder       = Base64.getUrlDecoder();
+    String         token;
+    String[]       chunks;
+    Base64.Decoder decoder = Base64.getUrlDecoder();
 
     public JWTChecker (String token) {
-        this.token=token;
-        chunks = token.split("\\.");
+        this.token = token;
+        if (!token.isEmpty())
+            chunks = token.split("\\.");
     }
 
     public void log () {
-        String header                = new String(decoder.decode(chunks[0]));
-        String payload               = new String(decoder.decode(chunks[1]));
+        if (chunks == null || chunks.length == 0) {
+            APILogger.logResult("EMPTY TOKEN");
+            return;
+        }
+        
+        String header  = new String(decoder.decode(chunks[0]));
+        String payload = new String(decoder.decode(chunks[1]));
 
         APILogger.logResult(header);
         APILogger.logResult(payload);
