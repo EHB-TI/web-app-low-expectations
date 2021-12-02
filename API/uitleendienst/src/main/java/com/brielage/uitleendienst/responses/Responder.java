2ecc82c51bf6d004a5195fc97d9d707f5976f8d2
Jens Wielfaert <jens.wielfaert@student.ehb.be>
2021-12-02 17:24:31 +0100

    Responder

---
+++ API/uitleendienst/src/main/java/com/brielage/uitleendienst/responses/Responder.java
@@ -0,0 +1,52 @@
+package com.brielage.uitleendienst.responses;
+
+import com.brielage.uitleendienst.tools.APILogger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@SuppressWarnings ("rawtypes")
+public enum Responder {
+    ;
+
+    public static ResponseEntity respondOk (Object o) {
+        APILogger.logSuccess(o.toString());
+        return ResponseEntity.ok()
+                             .body(o);
+    }
+
+    public static ResponseEntity respondOk (List<Object> objects) {
+        StringBuilder output = new StringBuilder();
+        for (Object o : objects)
+            output.append(o.toString())
+                  .append("; ");
+        APILogger.logSuccess(output.toString());
+        return ResponseEntity.ok()
+                             .body(objects);
+    }
+
+    public static ResponseEntity respondBadRequest (String reason) {
+        APILogger.logFail(reason);
+        return ResponseEntity.badRequest()
+                             .build();
+    }
+
+    public static ResponseEntity respondNotFound () {
+        APILogger.logFail("not found");
+        return ResponseEntity.notFound()
+                             .build();
+    }
+
+    @SuppressWarnings ("unchecked")
+    public static ResponseEntity respondCreated (Object o) {
+        APILogger.logSuccess(o.toString());
+        return new ResponseEntity(o, HttpStatus.CREATED);
+    }
+
+    public static ResponseEntity respondNoContent (String what) {
+        APILogger.logSuccess(what);
+        return ResponseEntity.noContent()
+                             .build();
+    }
+}
